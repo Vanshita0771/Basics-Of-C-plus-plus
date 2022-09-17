7783102af352cd7769a2e3dae920bf97f978a4d9
Vanshita0771 <78303804+Vanshita0771@users.noreply.github.com>
2022-09-17 12:44:35 +0530

    Create Function templates and static variables.cpp

---
+++ Generic Programming/Function templates and static variables.cpp
@@ -0,0 +1,24 @@
+/*Each instantiation of function template has its own copy of local static variables.
+For example, in the following program there are two instances: void fun(int ) and void fun(double ). So two copies of static variable i exist.*/
+#include <iostream>
+using namespace std;
+
+template <typename T>
+void fun(const T& x)
+{
+ static int i = 10;
+ cout << ++i;
+ return;
+}
+
+int main()
+{	
+ fun<int>(1); // prints 11
+ cout << endl;
+ fun<int>(2); // prints 12
+ cout << endl;
+ fun<double>(1.1); // prints 11
+ cout << endl;
+ getchar();
+ return 0;
+}
