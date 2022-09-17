b66782254b63648bafbf7b771255dc57e5320969
Vanshita0771 <78303804+Vanshita0771@users.noreply.github.com>
2022-09-17 12:03:06 +0530

    Create Passing default data type for template.cpp

---
+++ Generic Programming/Passing default data type for template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+template <class T,class U=float> U add(T a,U b){
+    return a+b;
+}
+int main()
+{
+    int a=2;
+    float b=8.7;
+    cout<<"Addition:"<<add<int>(a,1.3)<<endl;
+
+    return 0;
+}
