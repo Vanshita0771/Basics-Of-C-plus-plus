f3ded2d3b01c68c817ff82532f81d2d3662d74ff
Vanshita0771 <78303804+Vanshita0771@users.noreply.github.com>
2022-09-22 14:10:02 +0530

    Create Hollow Rhombus.cpp

---
+++ Patterns/Hollow Rhombus.cpp
@@ -0,0 +1,30 @@
+
+//Pattern 18 (Hollow Rhombus)
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n-i; j++){
+            cout<<" ";
+        }
+        for(int j=1; j<=n; j++){
+            if(i==1 || i==n){
+                cout<<"*";
+            }
+            else{
+                if(j==1 || j==n){
+                    cout<<"*";
+                }
+                else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<endl;
+    }
+}
+
