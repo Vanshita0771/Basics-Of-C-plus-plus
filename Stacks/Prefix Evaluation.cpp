b04c9dcb718793bdf3dbf303e626505f7bbe4617
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2022-09-27 15:31:29 +0530

    Create Prefix Evaluation.cpp

---
+++ Stacks/Prefix Evaluation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<stack>
+#include<cmath>
+using namespace std;
+int prefixEval(string s){
+    stack<int>st;
+    for(int i=s.length()-1;i>-1;i--){
+    if(s[i]>='0'&&s[i]<='9')
+     st.push(s[i]-'0');
+    else{
+        int a=st.top();
+        st.pop();
+        int b=st.top();
+        st.pop();
+        if(s[i]=='+')
+         st.push(a+b);
+         else if(s[i]=='-')
+         st.push(a-b);
+         else if(s[i]=='/')
+         st.push(a/b);
+         else if(s[i]=='*')
+         st.push(a*b);
+         else
+         st.push(pow(a,b));
+      }
+        
+    }
+    return st.top();
+}
+int main()
+{
+    string s;
+    cin>>s;
+    cout<<prefixEval(s)<<endl;
+    
+    return 0;
+}
+
+
