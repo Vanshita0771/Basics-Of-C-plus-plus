8a965f0982ab39fdcd576fadcf9eb94a9e92ce60
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2022-10-15 16:22:16 +0530

    Create Reverse A String.cpp

---
+++ Strings/Reverse A String.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str="Hello World";
+    int i=0;
+    int j=str.length()-1;
+    while(i<j){
+        swap(str[i],str[j]);
+        i++;
+        j--;
+    }
+    cout<<str<<endl;
+    return 0;
+}
